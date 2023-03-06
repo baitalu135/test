8f2727577693a5c33708e98ffc6ff40cc0f6b92a
baitalu135 <asano.ryota.19920919@gmail.com>
2023-03-06 10:25:57 +0900

    Update UnknownPlugin_delete.py

--- UnknownPlugin_delete.py
+++ UnknownPlugin_delete.py
@@ -2,8 +2,11 @@ import maya.cmds as cmds
 
 if cmds.unknownPlugin(q=True, list=True):
     plugins = cmds.unknownPlugin(q=True, list=True)
+    deleted_plugins = []
     for plugin in plugins:
         cmds.unknownPlugin(plugin, remove=True)
-    cmds.confirmDialog(title='Unknown Plugin', message=str(len(plugins))+' deleted', button=['OK'], defaultButton='OK')
+        deleted_plugins.append(plugin)
+    deleted_plugins_str = ', '.join(deleted_plugins)
+    cmds.confirmDialog(title='Unknown Plugin', message=deleted_plugins_str + ' deleted', button=['OK'], defaultButton='OK')
 else:
     cmds.confirmDialog(title='Unknown Plugin', message='no unknownPlugin', button=['OK'], defaultButton='OK')
